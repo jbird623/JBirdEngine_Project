3d5d57cb46dd39f3d4c58cb86ae781644ceeab9c
unknown <cirigj@SA2411-PC34.win.rpi.edu>
2015-12-09 19:29:02 -0500

    ++ Added bork things

---
+++ UnityProject/JBirdEngine/Assets/Scripts/ThrowErrors.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowErrors : MonoBehaviour {
+
+    void Start () {
+        Debug.LogError("Deleting System32.");
+        Debug.LogWarning("Warning: Universe currently imploding. Please advise.");
+        Debug.LogError("Error: Divide by 0. Are you trying to kill us all?");
+        Debug.LogError("This error message doesn't actually indicate any errors, it just wants attention.");
+        Debug.LogWarning("No one actually likes you. I'm sorry. Do you want a hug?");
+        Debug.LogWarning("Everyone knows your secret. Run.");
+        Debug.LogError("I'm out of witty error messages.");
+    }
+
+}
